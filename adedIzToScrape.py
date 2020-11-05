c7167881f31b773de77af8127fd4c6dce3044f29
Justin Ales <jma23@st-andrews.ac.uk>
2020-11-05 19:01:36 +0000

    Updated iz data for Nov 5th release to

--- adedIzToScrape.py
+++ adedIzToScrape.py
@@ -1,15 +1,20 @@
 import pandas as pd
 import datetime as dt
 
-cases =  pd.read_csv('scotland-iz-cases-from-aug.csv', thousands=',');
+# cases =  pd.read_csv('cases_from_nov5.csv', thousands=',')
+#
+# casesOlder = pd.read_csv('cases_from_nov4.csv', thousands=',')
+cases =  pd.read_csv('cases-20201026-20201102.csv', thousands=',')
 
-casesOlder = pd.read_csv('scotland_weekly_cases_iz.csv', thousands=',');
+casesOlder = pd.read_csv('scotland_weekly_cases_iz.csv', thousands=',')
 
 izNames = pd.read_csv('scotland-iz.csv')
 
 cases['dateEnd']=pd.to_datetime(cases['dateEnd'])
 casesOlder['dateEnd']=pd.to_datetime(casesOlder['dateEnd'])
-casesOlder=casesOlder.loc[casesOlder['dateEnd']<dt.datetime(2020,8,1)]
-cases=
-
- cases = pd.merge(left=cases,right=izNames[['IntZone','IntZoneName','CAName']],left_on=['council','IZ'], right_on=['CAName', 'IntZoneName'])
+casesOlder=casesOlder.loc[casesOlder['dateEnd']<min(cases['dateEnd'])]
+cases = cases.append(casesOlder)
+cases = pd.merge(left=cases,right=izNames[['IntZone','IntZoneName','CAName']],left_on=['council','IZ'], right_on=['CAName', 'IntZoneName'])
+cases['IZCode'] = cases['IntZone']
+cases['dateEnd']=cases['dateEnd'].dt.strftime('%d %B %Y')
+cases = cases[['IZCode','council','IZ','dateStart','dateEnd','cases','pop']]
